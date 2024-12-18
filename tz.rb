ae322580ab26034a7c4b3e7d293ed0c78763c05a
Steve Beville <steve_beville@hotmail.com>
2024-12-18 23:13:10 +0000

    GMT no longer requires blank subregion

--- tz.rb
+++ tz.rb
@@ -6,13 +6,13 @@ class TimezoneCLI
   def initialize
     @options = Hash.new
     map_timezones
-    select_target_region
-    select_target_subregion
+    select_input_type
+    make_selection
     change_timezone
   end
 
   def change_timezone
-    command = @target_subregion.empty? ? @target_region : [@target_region, @target_subregion].join('/')
+    command = @target_subregion.nil? || @target_subregion.empty? ? @target_region : [@target_region, @target_subregion].join('/')
     command_result = `sudo systemsetup -settimezone #{command} 2>&-`
     puts command_result
   end
@@ -24,6 +24,33 @@ class TimezoneCLI
 
   def select_target_region
     @target_region = TTY::Prompt.new.select("Select region:", @options.keys, per_page: 15)
+    puts "Selected region: #{@target_region}" # Debug statement to check the selected region
+  end
+
+  def make_selection
+    case @input_type
+    when "Recent"
+      # Add logic for "Recent" option
+      # Will need to remember recent selections
+      puts "`Recent` functionality not implemented yet."
+      select_target_region
+      select_target_subregion unless @target_region == "GMT"
+    when "By Region"
+      select_target_region
+      select_target_subregion unless @target_region == "GMT"
+    when "Search A-Z"
+      # Add logic for "Search A-Z" option
+      # maybe .map to filter timezones by letters entered
+      puts "`Search A-Z` functionality not implemented yet."
+      select_target_region
+      select_target_subregion unless @target_region == "GMT"
+    else
+      puts "Invalid input type selected."
+    end
+  end
+
+  def select_input_type
+    @input_type = TTY::Prompt.new.select("Select input type:", ["Recent", "By Region", "Search A-Z"], per_page: 3)
   end
 
   def map_timezones
